fd153f6b2c3d8867ce73653746f63f1c0982818e
jigjee <porntittaya.udm@gmail.com>
2022-07-31 14:37:34 +0700

    Intial project

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import UserRusult from "../components/users/UserRusult";
+import UserSearch from "../components/users/UserSearch";
+
+function Home() {
+  return (
+    <>
+      <UserSearch />
+      <UserRusult />
+    </>
+  );
+}
+
+export default Home;
